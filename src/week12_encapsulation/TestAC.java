d5c30abe714c1415063f5a42380e1c63b0abc8a8
sezimwil <sezimwilson@gmail.com>
2024-10-20 13:54:05 -0600

    added AC test class

---
+++ src/week12_encapsulation/TestAC.java
@@ -0,0 +1,8 @@
+package week12_encapsulation;
+
+public class TestAC {
+    public static void main(String[] args) {
+        AC ac = new AC(72, "On");
+        System.out.println("ac = " + ac);
+    }
+}
