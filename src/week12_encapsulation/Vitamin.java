1fc5cb2f08e36b5345a9f02ba8e7b142e30a7be4
sezimwil <sezimwilson@gmail.com>
2024-10-20 14:46:09 -0600

    added more classes to AC test

--- src/week12_encapsulation/Vitamin.java
+++ src/week12_encapsulation/Vitamin.java
@@ -3,10 +3,10 @@ public class Vitamin{
 private String name;
 
 public Vitamin(String name){
-this.name = nmae;
+this.name = name;
 }
 public void setName(String name){
-this.name = nmae;
+this.name = name;
 
 }
 }
